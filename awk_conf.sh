7442af4a66bdadf985ac02277c8e803eb37486de
756yang <1163700235@qq.com>
2023-11-12 20:29:24 +0800

    fix awk_conf

--- awk_conf.sh
+++ awk_conf.sh
@@ -4,8 +4,8 @@
 awk_script=$'{\n\tif(0);\n'
 end_script=$'END{\n'
 while [ $# -gt 0 ]; do
-	var_name="${1%% *}"
-	awk_script="$awk_script"$'\telse if(match($0,"^[^#]*'"$var_name"$' ")){\n'
+	var_name="${1%%[^_A-Za-z0-9]*}"
+	awk_script="$awk_script"$'\telse if(match($0,"^[^#]*'"$var_name"$'[^_A-Za-z0-9]")){\n'
 	awk_script="$awk_script"$'\t\t'"$var_name"$'="'"$1"$'";\n'
 	awk_script="$awk_script"$'\t\tprint '"$var_name"$';\n\t}\n'
 	end_script="$end_script"$'\tif(!length('"$var_name"$'))print "'"$1"$'";\n'
