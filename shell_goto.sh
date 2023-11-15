c53349859641ac6dca5f847386f6987ee98114bc
756yang <1163700235@qq.com>
2023-11-15 23:50:53 +0800

    fix shell_goto lead to can not read from stdin

--- shell_goto.sh
+++ shell_goto.sh
@@ -1,19 +1,17 @@
 # eval "$(cat shell_goto.sh)" label
-# eval "$(cat shell_goto.sh)" '"label:script"'
+# eval "$(cat shell_goto.sh)" label script
 :<<"EOT"
 # this is an example, echo boom forever
 #boom#
 echo boom
 eval "$(cat shell_goto.sh)" boom
 EOT
+function shell_goto ()
 {
-	IFS='' read -r shell_goto_jump
-	shell_goto_label="${shell_goto_jump%%:*}"
-	if [ "$shell_goto_label" = "$shell_goto_jump" ]; then
-		shell_goto_script="$0"
-	else
-		shell_goto_script="${shell_goto_jump#*:}"
-	fi
-	eval "$(sed -n "/^#$shell_goto_label#\$/{:a;n;p;b a};" "$shell_goto_script")"
+	[ $# -eq 0 -o $# -gt 2 ] && return 1
+	local label="$1"
+	[ $# -eq 1 ] && local script="$0" || local script="$2"
+	eval "$(sed -n "/^#$label#\$/{:a;n;p;b a};" "$script")"
 	exit
-} <<<
+}
+shell_goto
